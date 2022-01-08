2778e403609f16258f7145d1579c79eec9db60d9
kjun Yun <sound605@gmail.com>
2022-01-09 00:12:59 +0900

    Initial commit

---
+++ src/chap16/Lambda.java
@@ -0,0 +1,29 @@
+package chap16;
+
+public class Lambda {
+    public static void main(String[] args) {
+
+        FunctionInterface01 f = () -> System.out.println("FunctionInterface01 test");
+
+        f.test();
+
+        FunctionInterface02 f2 = (a) -> a;
+        System.out.println(f2.test("FunctionInterface02 test"));
+
+        FunctionInterface02 f3 = (a) -> {
+            return a;
+        };
+        System.out.println(f3.test("FunctionInterface02 test"));
+    }
+}
+
+//단 한 개의 추상 메서드를 갖고 있는 인터페이스
+@FunctionalInterface
+interface FunctionInterface01 {
+    public void test();
+}
+
+@FunctionalInterface
+interface FunctionInterface02 {
+    public String test(String a);
+}
