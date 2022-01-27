d2eb88db4f322a1ee924c86a3691e18c5cddfe6a
Yun Kyung Jun <sound605@gmail.com>
2022-01-27 14:24:04 +0900

    FunctionInterface Apply

--- src/Lambda/ConsumerFor.java
+++ src/Lambda/ConsumerFor.java
@@ -10,18 +10,18 @@ import java.util.function.Consumer;
 public class ConsumerFor {
     public static void main(String[] args) {
         List<Employee> emp = Arrays.asList(
-                new Employee(1, "펭우", 2400),
-                new Employee(2, "싱하", 2700),
-                new Employee(3, "성조", 3000),
-                new Employee(4, "마구", 3200),
-                new Employee(5, "조팍", 3400)
+                new Employee(1, "ABC", 2400),
+                new Employee(2, "DEF", 2700),
+                new Employee(3, "QWE", 3000),
+                new Employee(4, "ASD", 3200),
+                new Employee(5, "ZXC", 3400)
         );
 
         Consumer<Employee> consumer = x -> {
             x.setSalary(x.getSalary() * 2);
         };
 
-        System.out.println("== 연봉 2배 인상 ==");
+        System.out.println("== Double Salary ==");
         doubleSalary(emp, consumer.andThen(System.out::println));
     }
 
--- src/Lambda/ConsumerForEach.java
+++ src/Lambda/ConsumerForEach.java
@@ -6,11 +6,11 @@ import java.util.List;
 public class ConsumerForEach {
     public static void main(String[] args) {
         List<Employee> emp = Arrays.asList(
-                new Employee(1, "펭우", 2400),
-                new Employee(2, "싱하", 2700),
-                new Employee(3, "성조", 3000),
-                new Employee(4, "마구", 3200),
-                new Employee(5, "조팍", 3400)
+                new Employee(1, "ABC", 2400),
+                new Employee(2, "DEF", 2700),
+                new Employee(3, "QWE", 3000),
+                new Employee(4, "ASD", 3200),
+                new Employee(5, "ZXC", 3400)
         );
         emp.forEach(x -> {
             x.setSalary(x.getSalary() * 2);
